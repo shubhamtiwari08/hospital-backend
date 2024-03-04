8362ac8a060e54c2a4ee39868243203f3c83c27f
Shubham tiwari <115742355+shubhamtiwari08@users.noreply.github.com>
2024-03-04 10:04:59 +0000

    added ward update routes

--- Routes/wardRoutes.js
+++ Routes/wardRoutes.js
@@ -1,39 +1,51 @@
-const express = require('express');
-const app = express
-const Router = app.Router()
+const express = require("express");
+const app = express;
+const Router = app.Router();
 
-const { getWards, addWard, deleteWard, updateWard } = require("../controllers/wardController")
+const {
+  getWards,
+  addWard,
+  deleteWard,
+  updateWard,
+} = require("../controllers/wardController");
 
-Router.get('/', async (req, res) => {
+Router.get("/", async (req, res) => {
   try {
-    const wards = await getWards()
-    res.json({ wards })
+    const wards = await getWards();
+    res.json({ wards });
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
-Router.post('/', async (req, res) => {
-  console.log(req.body)
+Router.post("/", async (req, res) => {
+  console.log(req.body);
   try {
-    const wards = await addWard(req.body)
-    res.json({ wards })
+    const wards = await addWard(req.body);
+    res.json({ wards });
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
+});
 
-})
-
-
-Router.post('/delete/:id', async (req, res) => {
-  const id = req.params.id
+Router.post("/:id", async (req, res) => {
+  console.log(req.body);
   try {
-    const wards = await deleteWard(id)
-    res.json({ wards })
+    const wards = await updateWard(req.params.id, req.body);
+    res.json({ wards });
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
+});
 
-})
+Router.post("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const wards = await deleteWard(id);
+    res.json({ wards });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-module.exports = Router
+module.exports = Router;
